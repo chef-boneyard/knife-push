chef-boneyard/knife-push

commit 54ecc0ec84bd4caadfc62c1e76dd94bdb2306614

improve documentation, bump version

lib/chef/knife/job_start.rb
@@ -3,6 +3,8 @@ class Chef
     class JobStart < Chef::Knife
 
       deps do
+        require 'chef/rest'
+        require 'chef/node'
         require 'chef/search/query'
       end
 
@@ -18,41 +20,36 @@ class Chef
             :default => '100%',
             :description => 'Pushy job quorum. Percentage (-q 50%) or Count (-q 145).'
 
-      option :search,
+      option :query,
             :short => '-q QUERY',
             :long => '--query QUERY',
             :required => false,
             :description => 'Solr query for list of job candidates.'
 
       def run
+        @node_names = []
+        Chef::Log.info("running with #{config[:query]} ...")
         if config[:query]
           q = Chef::Search::Query.new
-          @type, @query = config[:query].split(/:/)
-          @escaped_query = URI.escape(@query,
+          @escaped_query = URI.escape(config[:query],
                                      Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
           begin
-            nodes = q.search(@type, @escaped_query)
+            nodes = q.search(:node, @escaped_query).first
           rescue Net::HTTPServerException => e
             msg Chef::JSONCompat.from_json(e.response.body)['error'].first
             ui.error("knife search failed: #{msg}")
             exit 1
           end
-          nodes.each do |node|
-            unless node.kind_of?(Chef::Node)
-              Chef::Log.error('Invalid search query.')
-              exit 1
-            end
-            nodes << node.name
-          end
+          nodes.each { |node| @node_names << node[:hostname] unless node[:hostname].nil? }
         else
-          nodes = name_args[1,name_args.length-1]
+          @node_names = name_args[1,name_args.length-1]
         end
 
         rest = Chef::REST.new(Chef::Config[:chef_server_url])
 
         job_json = {
           'command' => name_args[0],
-          'nodes' => nodes,
+          'nodes' => @node_names,
           'quorum' => get_quorum(config[:quorum], nodes.length)
         }
         job_json['run_timeout'] = config[:run_timeout].to_i if config[:run_timeout]

Gemfile
@@ -2,5 +2,3 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in knife-pushy.gemspec
 gemspec
-
-gem 'knife-pushy', :git => 'git://github.com/timops/knife-pushy.git'

README.rdoc
@@ -1,10 +1,79 @@
-== Knife Pushy
+= Knife Pushy
 
 = DESCRIPTION:
 
-Eleventy hundred kajillion foot-pounds of awesome.
+The knife pushy plugin is used by the Chef workstation to interact with the Pushy API to start jobs, view job status, view job lists, and view node status.
 
-= USAGE:
+= INSTALLATION:
+
+To build and install the plugin, run:
 
     rake install
-    knife pushy status
+
+= CONFIGURATION:
+
+If pushy server is running on the same host as the OPC, then no reconfiguration is required on the Chef workstation.
+
+= SUBCOMMANDS:
+
+This plugin provides the following Knife subcommands.  Specific command options can be found by invoking the subcommand with a <tt>--help</tt> flag.
+
+== job list
+
+The <tt>job list</tt> subcommand is used to view a list of Pushy jobs.
+
+== Syntax
+  $ knife job list
+
+== job start
+
+The <tt>job start</tt> subcommand is used to start a Pushy job.
+
+== Syntax
+  $ knife job start (options) COMMAND [NODE, NODE, ...]
+
+== Options
+This argument has the following options:
+
+  --timeout TIMEOUT
+    The maximum amount of time (in seconds) by which a job must complete, before it will be stopped.
+
+  -q QUORUM --quorum QUORUM
+    The minimum number of nodes that match the search criteria, are available, and acknowledge the job request. This can be expressed as a 
+    percentage (e.g. 50%) or as an absolute number of nodes (e.g. 145). Default value: 100%
+
+== Examples
+For example, to search for nodes assigned the role “webapp”, and where 90% of those nodes must be available, enter:
+
+  $ knife job start -quorum 90% 'chef-client' --search 'role:webapp'
+
+To search for a specific set of nodes (named chico, harpo, groucho, gummo, zeppo), and where 90% of those nodes must be available, enter:
+
+  $ knife job start --quorum 90% 'chef-client' chico harpo groucho gummo zeppo
+
+Use the <tt>knife job start</tt> subcommand to run a job with the following syntax:
+
+  $ knife job start job_name node_name
+
+For example, to run a job named add-glasses against a node named “ricardosalazar”, enter the following:
+
+  $ knife job start add-glasses 'ricardosalazar'
+
+== job status
+
+The <tt>job status</tt> argument is used to view the status of Pushy jobs.
+
+== Syntax
+  $ knife job status
+
+== Examples
+For example, to view the status of a job that has the identifier of “235”, enter:
+
+  $ knife job status 235
+
+== node status
+
+The <tt>node status</tt> argument is used to identify nodes that Pushy may interact with.
+
+== Syntax
+  $ knife node status

knife-pushy.gemspec
@@ -2,11 +2,11 @@ $:.unshift(File.dirname(__FILE__) + '/lib')
 
 Gem::Specification.new do |s|
   s.name = "knife-pushy"
-  s.version = '0.1'
+  s.version = '0.2'
   s.platform = Gem::Platform::RUBY
   s.has_rdoc = true
   s.extra_rdoc_files = ["README.rdoc", "LICENSE"]
-  s.summary = "Bork bork bork"
+  s.summary = "Knife plugin for OPC pushy"
   s.description = s.summary
   s.author = "John Keiser"
   s.email = "jkeiser@opscode.com"
@@ -17,6 +17,7 @@ Gem::Specification.new do |s|
   # can be included with apps that have restrictive Gemfile.locks.
   # s.add_dependency "mixlib-cli", ">= 1.2.2"
 
+  s.add_dependency 'chef', '~> 11.6.0'
   s.require_path = 'lib'
   s.files = %w(LICENSE README.rdoc Rakefile) + Dir.glob("{lib,spec}/**/*")
 end

commit 056d1f39c5eac5d62b88c3108b7c39913eb39d5f

use node names not hostnames in job dispatch
Commit 54ecc0e introduced a bug where the nodes from the search
results would be collected by hostname rather than node name. Push
jobs are dispatched to nodes by their `Chef::Node` name.

lib/chef/knife/job_start.rb
@@ -56,7 +56,7 @@ class Chef
             ui.error("knife search failed: #{msg}")
             exit 1
           end
-          nodes.each { |node| @node_names << node[:hostname] unless node[:hostname].nil? }
+          nodes.each { |node| @node_names << node.name }
         else
           @node_names = name_args[1,name_args.length-1]
         end